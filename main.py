8c8e7842557dc3a5b44a32134b36f52086fa5f53
Supreme_Engineer98 <gmavropouloscoding@gmail.com>
2026-09-23 00:15:25 +0300

    Feat:created the chatbot method which exchanges messages with the agent

---
+++ main.py
@@ -0,0 +1,12 @@
+### the main file which calls the agent to start
+from agent.agent import Agent
+
+#main class
+class main():
+    def __init__(self):
+        pass
+
+    #create an agent's object
+    agent = Agent()
+    #call the chatbot method to chat
+    agent.chatbot()
